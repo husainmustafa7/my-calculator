9a12df6d57bfff462e515ef9296d8fea63990ffd
Mustafa Husain Siddiqui <mhs@Mustafas-Mac-mini.local>
2025-08-19 17:30:42 +0530

    Updates

---
+++ static/js/851.6a3548ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_calculator=self.webpackChunkmy_calculator||[]).push([[851],{851:(a,e,t)=>{t.r(e),t.d(e,{ShareWeb:()=>n});var r=t(403);class n extends r.E_{async canShare(){return"undefined"!==typeof navigator&&navigator.share?{value:!0}:{value:!1}}async share(a){if("undefined"===typeof navigator||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}}}}]);
+//# sourceMappingURL=851.6a3548ab.chunk.js.map
